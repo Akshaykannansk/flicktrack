8451e0725d269a4c82139d6cdaeae9586b100eef
Akshay kannan <Akshaykannansrt10@gmail.com>
2025-07-21 13:32:10 +0000

    generator client {   provider      = "prisma-client-js"   binaryTargets

--- app/wait-for-postgres.sh
+++ app/wait-for-postgres.sh
@@ -7,10 +7,13 @@ host="$1"
 shift
 cmd="$@"
 
+# Loop until we can successfully connect to Postgres
 until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
 
 >&2 echo "Postgres is up - executing command"
+# Run migrations first, then execute the main command (e.g., `npm run dev`)
+npm run db:migrate
 exec $cmd
