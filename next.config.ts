472f8dc75cfc1a8bb31172d1c248225b1222d0e7
Akshay kannan <Akshaykannansrt10@gmail.com>
2025-07-26 10:00:32 +0000

    fix the issue in next.config.ts allwoedOrigins

--- next.config.ts
+++ next.config.ts
@@ -24,9 +24,11 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  allowedDevOrigins: [
-    "*"
-  ],
+ experimental:{
+  serverActions:{
+    allowedOrigins: ['*']
+  }
+ }
 };
 
 export default nextConfig;
