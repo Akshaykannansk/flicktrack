9c4aac29b279f9161c7887e03a6463423524182d
Akshay kannan <Akshaykannansrt10@gmail.com>
2025-08-08 11:35:49 +0000

    profile edit page createserverclient issue fix

--- src/app/profile/edit/page.tsx
+++ src/app/profile/edit/page.tsx
@@ -45,19 +45,23 @@ async function getEditProfileData(userId: string) {
 
 export default async function EditProfilePage() {
     const cookieStore = await cookies();
-    const supabase = createServerClient({
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
+    const supabase = createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+        cookies: {
+            get(name: string) {
+            return cookieStore.get(name)?.value
+            },
+            set(name: string, value: string, options: CookieOptions) {
+            cookieStore.set({ name, value, ...options })
+            },
+            remove(name: string, options: CookieOptions) {
+            cookieStore.set({ name, value: '', ...options })
+            },
         },
-        set(name: string, value: string, options: CookieOptions) {
-          cookieStore.set({ name, value, ...options })
-        },
-        remove(name: string, options: CookieOptions) {
-          cookieStore.set({ name, value: '', ...options })
-        },
-      },
-    });
+    }
+    );
     const { data: { session } } = await supabase.auth.getSession();
     const user = session?.user;
 
